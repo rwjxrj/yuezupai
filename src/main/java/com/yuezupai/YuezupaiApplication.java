6e368aba93b1a1b9a24f08d397f2b27eb45573b5
rwjxrj <2206232778@qq.com>
2026-03-13 00:20:37 +0800

    悦租派java框架
    # Conflicts:
    #	pom.xml

---
+++ src/main/java/com/yuezupai/YuezupaiApplication.java
@@ -0,0 +1,13 @@
+package com.yuezupai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YuezupaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YuezupaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhang/yuezupai/YuezupaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhang.yuezupai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YuezupaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
